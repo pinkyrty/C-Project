385599b7989dcdcd5e90297897a444b7ea24290e
Alvin <alvinlam0730@gmail.com>
2025-05-27 23:07:19 +0800

    改STYLE

--- CreateUserForm.Designer.cs
+++ CreateUserForm.Designer.cs
@@ -30,62 +30,53 @@
         {
             cbbDepartment = new ComboBox();
             btnConfirm = new Button();
-            lblFirstName = new Label();
-            lblName = new Label();
+            lblUserName = new Label();
             lblUserID = new Label();
             lblPassword = new Label();
             lblConfirm = new Label();
-            cbbPermission = new ComboBox();
             txtName = new TextBox();
             txtUserID = new TextBox();
             txtPassword = new TextBox();
             txtConfirm = new TextBox();
-            txtFirstName = new TextBox();
-            lblPermission = new Label();
             lblDepartment = new Label();
             pictureBox1 = new PictureBox();
+            lblCreateUser = new Label();
+            btnCancel = new Button();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // cbbDepartment
             // 
+            cbbDepartment.DropDownStyle = ComboBoxStyle.DropDownList;
             cbbDepartment.FormattingEnabled = true;
-            cbbDepartment.Location = new Point(275, 253);
+            cbbDepartment.Items.AddRange(new object[] { "Sales Department", "R&D Department", "Production Department", "Inventory / Logistics Department", "Customer Service Department", "Finance Department", "IT Department" });
+            cbbDepartment.Location = new Point(272, 286);
             cbbDepartment.Name = "cbbDepartment";
             cbbDepartment.Size = new Size(129, 23);
             cbbDepartment.TabIndex = 0;
             // 
             // btnConfirm
             // 
-            btnConfirm.Location = new Point(275, 360);
+            btnConfirm.Location = new Point(297, 350);
             btnConfirm.Name = "btnConfirm";
             btnConfirm.Size = new Size(75, 23);
             btnConfirm.TabIndex = 1;
             btnConfirm.Text = "Confirm";
             btnConfirm.UseVisualStyleBackColor = true;
             // 
-            // lblFirstName
+            // lblUserName
             // 
-            lblFirstName.AutoSize = true;
-            lblFirstName.Location = new Point(172, 56);
-            lblFirstName.Name = "lblFirstName";
-            lblFirstName.Size = new Size(67, 15);
-            lblFirstName.TabIndex = 2;
-            lblFirstName.Text = "First Name";
-            // 
-            // lblName
-            // 
-            lblName.AutoSize = true;
-            lblName.Location = new Point(195, 95);
-            lblName.Name = "lblName";
-            lblName.Size = new Size(42, 15);
-            lblName.TabIndex = 3;
-            lblName.Text = "Name";
+            lblUserName.AutoSize = true;
+            lblUserName.Location = new Point(166, 128);
+            lblUserName.Name = "lblUserName";
+            lblUserName.Size = new Size(70, 15);
+            lblUserName.TabIndex = 3;
+            lblUserName.Text = "User Name";
             // 
             // lblUserID
             // 
             lblUserID.AutoSize = true;
-            lblUserID.Location = new Point(195, 137);
+            lblUserID.Location = new Point(192, 170);
             lblUserID.Name = "lblUserID";
             lblUserID.Size = new Size(44, 15);
             lblUserID.TabIndex = 4;
@@ -94,7 +85,7 @@
             // lblPassword
             // 
             lblPassword.AutoSize = true;
-            lblPassword.Location = new Point(179, 177);
+            lblPassword.Location = new Point(176, 210);
             lblPassword.Name = "lblPassword";
             lblPassword.Size = new Size(60, 15);
             lblPassword.TabIndex = 5;
@@ -103,68 +94,51 @@
             // lblConfirm
             // 
             lblConfirm.AutoSize = true;
-            lblConfirm.Location = new Point(131, 215);
+            lblConfirm.Location = new Point(128, 248);
             lblConfirm.Name = "lblConfirm";
             lblConfirm.Size = new Size(108, 15);
             lblConfirm.TabIndex = 6;
             lblConfirm.Text = "Confirm Password";
             // 
-            // cbbPermission
-            // 
-            cbbPermission.FormattingEnabled = true;
-            cbbPermission.Location = new Point(275, 289);
-            cbbPermission.Name = "cbbPermission";
-            cbbPermission.Size = new Size(129, 23);
-            cbbPermission.TabIndex = 7;
-            // 
             // txtName
             // 
-            txtName.Location = new Point(275, 95);
+            txtName.Location = new Point(272, 128);
+            txtName.MaxLength = 32;
             txtName.Name = "txtName";
             txtName.Size = new Size(129, 23);
             txtName.TabIndex = 8;
             // 
             // txtUserID
             // 
-            txtUserID.Location = new Point(275, 137);
+            txtUserID.Location = new Point(272, 170);
             txtUserID.Name = "txtUserID";
+            txtUserID.ReadOnly = true;
             txtUserID.Size = new Size(129, 23);
             txtUserID.TabIndex = 9;
+            txtUserID.TabStop = false;
             // 
             // txtPassword
             // 
-            txtPassword.Location = new Point(275, 174);
+            txtPassword.Location = new Point(272, 207);
+            txtPassword.MaxLength = 32;
             txtPassword.Name = "txtPassword";
             txtPassword.Size = new Size(129, 23);
             txtPassword.TabIndex = 10;
+            txtPassword.UseSystemPasswordChar = true;
             // 
             // txtConfirm
             // 
-            txtConfirm.Location = new Point(275, 212);
+            txtConfirm.Location = new Point(272, 245);
+            txtConfirm.MaxLength = 32;
             txtConfirm.Name = "txtConfirm";
             txtConfirm.Size = new Size(129, 23);
             txtConfirm.TabIndex = 11;
-            // 
-            // txtFirstName
-            // 
-            txtFirstName.Location = new Point(275, 56);
-            txtFirstName.Name = "txtFirstName";
-            txtFirstName.Size = new Size(129, 23);
-            txtFirstName.TabIndex = 12;
-            // 
-            // lblPermission
-            // 
-            lblPermission.AutoSize = true;
-            lblPermission.Location = new Point(172, 292);
-            lblPermission.Name = "lblPermission";
-            lblPermission.Size = new Size(67, 15);
-            lblPermission.TabIndex = 13;
-            lblPermission.Text = "Permission";
+            txtConfirm.UseSystemPasswordChar = true;
             // 
             // lblDepartment
             // 
             lblDepartment.AutoSize = true;
-            lblDepartment.Location = new Point(164, 253);
+            lblDepartment.Location = new Point(161, 286);
             lblDepartment.Name = "lblDepartment";
             lblDepartment.Size = new Size(75, 15);
             lblDepartment.TabIndex = 14;
@@ -181,30 +155,49 @@
             pictureBox1.TabIndex = 15;
             pictureBox1.TabStop = false;
             // 
+            // lblCreateUser
+            // 
+            lblCreateUser.AutoSize = true;
+            lblCreateUser.Font = new Font("Microsoft JhengHei UI", 15F);
+            lblCreateUser.Location = new Point(206, 50);
+            lblCreateUser.Name = "lblCreateUser";
+            lblCreateUser.Size = new Size(119, 25);
+            lblCreateUser.TabIndex = 16;
+            lblCreateUser.Text = "Create User";
+            // 
+            // btnCancel
+            // 
+            btnCancel.Location = new Point(146, 350);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(75, 23);
+            btnCancel.TabIndex = 17;
+            btnCancel.Text = "Cancel";
+            btnCancel.UseVisualStyleBackColor = true;
+            btnCancel.Click += button1_Click;
+            // 
             // CreateUserForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.SlateGray;
             ClientSize = new Size(474, 429);
+            Controls.Add(btnCancel);
+            Controls.Add(lblCreateUser);
             Controls.Add(pictureBox1);
             Controls.Add(lblDepartment);
-            Controls.Add(lblPermission);
-            Controls.Add(txtFirstName);
             Controls.Add(txtConfirm);
             Controls.Add(txtPassword);
             Controls.Add(txtUserID);
             Controls.Add(txtName);
-            Controls.Add(cbbPermission);
             Controls.Add(lblConfirm);
             Controls.Add(lblPassword);
             Controls.Add(lblUserID);
-            Controls.Add(lblName);
-            Controls.Add(lblFirstName);
+            Controls.Add(lblUserName);
             Controls.Add(btnConfirm);
             Controls.Add(cbbDepartment);
             Name = "CreateUserForm";
-            Text = "CreateUserForm";
+            Text = "CreateUser";
+            Load += CreateUserForm_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
@@ -215,18 +208,18 @@
         private ComboBox cbbDepartment;
         private Button btnConfirm;
         private Label lblFirstName;
-        private Label lblName;
+        private Label lblUserName;
         private Label lblUserID;
         private Label lblPassword;
         private Label lblConfirm;
-        private ComboBox cbbPermission;
         private TextBox txtName;
         private TextBox txtUserID;
         private TextBox txtPassword;
         private TextBox txtConfirm;
         private TextBox txtFirstName;
-        private Label lblPermission;
         private Label lblDepartment;
         private PictureBox pictureBox1;
+        private Label lblCreateUser;
+        private Button btnCancel;
     }
 }

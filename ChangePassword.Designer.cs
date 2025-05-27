e1d2fa68bc54a97224a0013d66b16391f3e055a4
harry4867 <liyuhin123@gmail.com>
2025-05-27 20:46:07 +0800

    re six img

--- ChangePassword.Designer.cs
+++ ChangePassword.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ChangePassword));
             label1 = new Label();
             label2 = new Label();
             DepartmentDisplay = new Label();
@@ -41,12 +42,14 @@
             ConfirmPasswordBox = new TextBox();
             UpdatePasswordButton = new Button();
             CancelButton = new Button();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(92, 43);
+            label1.Location = new Point(111, 47);
             label1.Name = "label1";
             label1.Size = new Size(71, 15);
             label1.TabIndex = 0;
@@ -55,7 +58,7 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(92, 93);
+            label2.Location = new Point(111, 97);
             label2.Name = "label2";
             label2.Size = new Size(75, 15);
             label2.TabIndex = 1;
@@ -64,7 +67,7 @@
             // DepartmentDisplay
             // 
             DepartmentDisplay.AutoSize = true;
-            DepartmentDisplay.Location = new Point(314, 93);
+            DepartmentDisplay.Location = new Point(265, 97);
             DepartmentDisplay.Name = "DepartmentDisplay";
             DepartmentDisplay.Size = new Size(0, 15);
             DepartmentDisplay.TabIndex = 2;
@@ -72,7 +75,7 @@
             // StaffNameDisplay
             // 
             StaffNameDisplay.AutoSize = true;
-            StaffNameDisplay.Location = new Point(314, 43);
+            StaffNameDisplay.Location = new Point(265, 47);
             StaffNameDisplay.Name = "StaffNameDisplay";
             StaffNameDisplay.Size = new Size(0, 15);
             StaffNameDisplay.TabIndex = 3;
@@ -80,7 +83,7 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(162, 145);
+            label3.Location = new Point(175, 149);
             label3.Name = "label3";
             label3.Size = new Size(108, 15);
             label3.TabIndex = 4;
@@ -90,7 +93,7 @@
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(60, 192);
+            label4.Location = new Point(73, 196);
             label4.Name = "label4";
             label4.Size = new Size(107, 15);
             label4.TabIndex = 5;
@@ -98,7 +101,7 @@
             // 
             // ExistingPasswordBox
             // 
-            ExistingPasswordBox.Location = new Point(240, 189);
+            ExistingPasswordBox.Location = new Point(253, 193);
             ExistingPasswordBox.Name = "ExistingPasswordBox";
             ExistingPasswordBox.Size = new Size(159, 23);
             ExistingPasswordBox.TabIndex = 6;
@@ -106,7 +109,7 @@
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(60, 244);
+            label5.Location = new Point(73, 248);
             label5.Name = "label5";
             label5.Size = new Size(90, 15);
             label5.TabIndex = 7;
@@ -114,7 +117,7 @@
             // 
             // NewPasswordBox
             // 
-            NewPasswordBox.Location = new Point(240, 241);
+            NewPasswordBox.Location = new Point(253, 245);
             NewPasswordBox.Name = "NewPasswordBox";
             NewPasswordBox.Size = new Size(159, 23);
             NewPasswordBox.TabIndex = 8;
@@ -122,7 +125,7 @@
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(60, 294);
+            label6.Location = new Point(73, 298);
             label6.Name = "label6";
             label6.Size = new Size(109, 15);
             label6.TabIndex = 9;
@@ -130,14 +133,14 @@
             // 
             // ConfirmPasswordBox
             // 
-            ConfirmPasswordBox.Location = new Point(240, 291);
+            ConfirmPasswordBox.Location = new Point(253, 295);
             ConfirmPasswordBox.Name = "ConfirmPasswordBox";
             ConfirmPasswordBox.Size = new Size(159, 23);
             ConfirmPasswordBox.TabIndex = 10;
             // 
             // UpdatePasswordButton
             // 
-            UpdatePasswordButton.Location = new Point(75, 362);
+            UpdatePasswordButton.Location = new Point(88, 366);
             UpdatePasswordButton.Name = "UpdatePasswordButton";
             UpdatePasswordButton.Size = new Size(125, 35);
             UpdatePasswordButton.TabIndex = 11;
@@ -146,12 +149,23 @@
             // 
             // CancelButton
             // 
-            CancelButton.Location = new Point(240, 362);
+            CancelButton.Location = new Point(253, 366);
             CancelButton.Name = "CancelButton";
             CancelButton.Size = new Size(125, 35);
             CancelButton.TabIndex = 12;
             CancelButton.Text = "Cancel";
             CancelButton.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(13, 34);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(86, 78);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 13;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
             // 
             // ChangePassword
             // 
@@ -159,6 +173,7 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.SlateGray;
             ClientSize = new Size(469, 484);
+            Controls.Add(pictureBox1);
             Controls.Add(CancelButton);
             Controls.Add(UpdatePasswordButton);
             Controls.Add(ConfirmPasswordBox);
@@ -175,6 +190,7 @@
             Name = "ChangePassword";
             Text = "ChangePassword";
             Load += ChangePassword_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -194,5 +210,6 @@
         private TextBox ConfirmPasswordBox;
         private Button UpdatePasswordButton;
         private Button CancelButton;
+        private PictureBox pictureBox1;
     }
 }
--- ChangePassword.cs
+++ ChangePassword.cs
@@ -26,5 +26,10 @@ namespace C_Project
         {
 
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
